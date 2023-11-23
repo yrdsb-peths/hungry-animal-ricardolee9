82cb874393b35de6182f34fd0bcd3227747c4c54
Ricardo <62413909+Ricardolcm888@users.noreply.github.com>
2023-11-22 20:57:44 -0500

    Added Animal and Allows the Animal to Move

---
+++ Snake.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Snake here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Snake extends Actor
+{
+    /**
+     * Act - do whatever the Snake wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if (Greenfoot.isKeyDown("a")) {
+            move(-1);
+        }
+        if (Greenfoot.isKeyDown("d")) {
+            move(1);
+        }
+    }
+}
