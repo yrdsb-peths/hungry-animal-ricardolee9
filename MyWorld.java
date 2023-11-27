471ed1d2bc611cf6a0efe31e84487fcbe7667525
Ricardo <440000735@gapps.yrdsb.ca>
2023-11-27 08:43:55 -0500

    Added Comment

--- MyWorld.java
+++ MyWorld.java
@@ -8,6 +8,7 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        // Create a world that has no boundaries
         super(600, 400, 1, false);
         
         // Create the Animal
