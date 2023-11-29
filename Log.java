763f9f9e9dd7c3b71e63b7e48aea3350651eb230
Ricardo <440000735@gapps.yrdsb.ca>
2023-11-29 08:33:36 -0500

    Added Sound and Overloaded info method

--- Log.java
+++ Log.java
@@ -1,12 +1,30 @@
 public class Log  
 {
-    private static boolean debug = true;
-    
+    private static boolean debug = false;
+
     public Log()
     {
     }
-    
+
     public static void info(String text) {
+        if (debug) {
+            System.out.println(text);
+        }
+    }
+
+    public static void info(boolean text) {
+        if (debug) {
+            System.out.println(text);
+        }
+    }
+
+    public static void info(int text) {
+        if (debug) {
+            System.out.println(text);
+        }
+    }
+
+    public static void info(Object text) {
         if (debug) {
             System.out.println(text);
         }
